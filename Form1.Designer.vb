490b2ba7081ac15c0e89319bb999d98c7df1ed9b
Radif Dahlin Rahmana <64096815+RadifDahlin1994@users.noreply.github.com>
2020-05-14 20:07:39 +0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,311 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Texthadir = New System.Windows.Forms.TextBox()
+        Me.Texttugas = New System.Windows.Forms.TextBox()
+        Me.Textuts = New System.Windows.Forms.TextBox()
+        Me.Textuas = New System.Windows.Forms.TextBox()
+        Me.Textangka = New System.Windows.Forms.TextBox()
+        Me.proses = New System.Windows.Forms.Button()
+        Me.hapus = New System.Windows.Forms.Button()
+        Me.keluar = New System.Windows.Forms.Button()
+        Me.Textnama = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Textnpm = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Texthuruf = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Textket = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(61, 142)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nilai Hadir"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(61, 176)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(88, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nilai Tugas"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(61, 213)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(75, 16)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Nilai UTS"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(61, 251)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(75, 16)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Nilai UAS"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(61, 346)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(87, 16)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Keterangan"
+        '
+        'Texthadir
+        '
+        Me.Texthadir.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Texthadir.Location = New System.Drawing.Point(191, 137)
+        Me.Texthadir.Name = "Texthadir"
+        Me.Texthadir.Size = New System.Drawing.Size(100, 20)
+        Me.Texthadir.TabIndex = 5
+        '
+        'Texttugas
+        '
+        Me.Texttugas.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Texttugas.Location = New System.Drawing.Point(191, 172)
+        Me.Texttugas.Name = "Texttugas"
+        Me.Texttugas.Size = New System.Drawing.Size(100, 20)
+        Me.Texttugas.TabIndex = 6
+        '
+        'Textuts
+        '
+        Me.Textuts.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Textuts.Location = New System.Drawing.Point(191, 209)
+        Me.Textuts.Name = "Textuts"
+        Me.Textuts.Size = New System.Drawing.Size(100, 20)
+        Me.Textuts.TabIndex = 7
+        '
+        'Textuas
+        '
+        Me.Textuas.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Textuas.Location = New System.Drawing.Point(191, 247)
+        Me.Textuas.Name = "Textuas"
+        Me.Textuas.Size = New System.Drawing.Size(100, 20)
+        Me.Textuas.TabIndex = 8
+        '
+        'Textangka
+        '
+        Me.Textangka.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.Textangka.Location = New System.Drawing.Point(191, 306)
+        Me.Textangka.Name = "Textangka"
+        Me.Textangka.Size = New System.Drawing.Size(100, 20)
+        Me.Textangka.TabIndex = 9
+        '
+        'proses
+        '
+        Me.proses.BackColor = System.Drawing.Color.Lime
+        Me.proses.Location = New System.Drawing.Point(361, 157)
+        Me.proses.Name = "proses"
+        Me.proses.Size = New System.Drawing.Size(96, 110)
+        Me.proses.TabIndex = 10
+        Me.proses.Text = "Proses"
+        Me.proses.UseVisualStyleBackColor = False
+        '
+        'hapus
+        '
+        Me.hapus.BackColor = System.Drawing.Color.Yellow
+        Me.hapus.Location = New System.Drawing.Point(191, 388)
+        Me.hapus.Name = "hapus"
+        Me.hapus.Size = New System.Drawing.Size(100, 35)
+        Me.hapus.TabIndex = 11
+        Me.hapus.Text = "Hapus"
+        Me.hapus.UseVisualStyleBackColor = False
+        '
+        'keluar
+        '
+        Me.keluar.BackColor = System.Drawing.Color.Red
+        Me.keluar.Location = New System.Drawing.Point(354, 388)
+        Me.keluar.Name = "keluar"
+        Me.keluar.Size = New System.Drawing.Size(100, 35)
+        Me.keluar.TabIndex = 12
+        Me.keluar.Text = "Keluar"
+        Me.keluar.UseVisualStyleBackColor = False
+        '
+        'Textnama
+        '
+        Me.Textnama.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Textnama.Location = New System.Drawing.Point(191, 102)
+        Me.Textnama.Name = "Textnama"
+        Me.Textnama.Size = New System.Drawing.Size(266, 20)
+        Me.Textnama.TabIndex = 14
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(61, 106)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(49, 16)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Nama"
+        '
+        'Textnpm
+        '
+        Me.Textnpm.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Textnpm.Location = New System.Drawing.Point(191, 66)
+        Me.Textnpm.Name = "Textnpm"
+        Me.Textnpm.Size = New System.Drawing.Size(100, 20)
+        Me.Textnpm.TabIndex = 16
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(61, 70)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(40, 16)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "Npm"
+        '
+        'Texthuruf
+        '
+        Me.Texthuruf.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.Texthuruf.Location = New System.Drawing.Point(354, 306)
+        Me.Texthuruf.Name = "Texthuruf"
+        Me.Texthuruf.Size = New System.Drawing.Size(100, 20)
+        Me.Texthuruf.TabIndex = 17
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(188, 287)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(88, 16)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "Nilai Angka"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(351, 287)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(80, 16)
+        Me.Label9.TabIndex = 19
+        Me.Label9.Text = "Nilai Huruf"
+        '
+        'Textket
+        '
+        Me.Textket.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Textket.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Textket.Location = New System.Drawing.Point(191, 332)
+        Me.Textket.Multiline = True
+        Me.Textket.Name = "Textket"
+        Me.Textket.Size = New System.Drawing.Size(263, 50)
+        Me.Textket.TabIndex = 20
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Monotype Corsiva", 21.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.ForeColor = System.Drawing.Color.Yellow
+        Me.Label10.Location = New System.Drawing.Point(98, 9)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(348, 36)
+        Me.Label10.TabIndex = 21
+        Me.Label10.Text = "Menghitung Nilai Mahasiswa"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Highlight
+        Me.ClientSize = New System.Drawing.Size(547, 445)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Textket)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Texthuruf)
+        Me.Controls.Add(Me.Textnpm)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Textnama)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.keluar)
+        Me.Controls.Add(Me.hapus)
+        Me.Controls.Add(Me.proses)
+        Me.Controls.Add(Me.Textangka)
+        Me.Controls.Add(Me.Textuas)
+        Me.Controls.Add(Me.Textuts)
+        Me.Controls.Add(Me.Texttugas)
+        Me.Controls.Add(Me.Texthadir)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "IF JAMAK"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Texthadir As TextBox
+    Friend WithEvents Texttugas As TextBox
+    Friend WithEvents Textuts As TextBox
+    Friend WithEvents Textuas As TextBox
+    Friend WithEvents Textangka As TextBox
+    Friend WithEvents proses As Button
+    Friend WithEvents hapus As Button
+    Friend WithEvents keluar As Button
+    Friend WithEvents Textnama As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Textnpm As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Texthuruf As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Textket As TextBox
+    Friend WithEvents Label10 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form1
+    Private Sub proses_Click(sender As Object, e As EventArgs) Handles proses.Click
+        Dim npm As String
+        Dim nama As String
+        Dim hadir As Integer
+        Dim tugas As Integer
+        Dim uts As Integer
+        Dim uas As Integer
+        Dim nilaihuruf As String
+        Dim nilaiangka As Integer
+        Dim ket As String
+        npm = Textnpm.Text
+        nama = Textnama.Text
+        hadir = Val(Texthadir.Text)
+        tugas = Val(Texttugas.Text)
+        uts = Val(Textuts.Text)
+        uas = Val(Textuas.Text)
+        nilaiangka = (0.1 * hadir + 0.2 * tugas + 0.3 * uts + 0.4 * uas)
+        Textangka.Text = nilaiangka
+
+        If nilaiangka <= 50 Then
+            nilaihuruf = "E"
+            ket = "Nilai Anda sangat kurang! Maaf, Anda Gagal! :'("
+        ElseIf nilaiangka <= 50 Then
+            nilaihuruf = "D"
+            ket = "Nilai Anda Kurang! Maaf,Anda Gagal! :'("
+        ElseIf nilaiangka <= 60 Then
+            nilaihuruf = "C"
+            ket = "Nilai Anda cukup! Anda Lulus,Tingkakan lagi!"
+        ElseIf nilaiangka <= 66 Then
+            nilaihuruf = "C+"
+            ket = "Nilai Anda cukup! Anda Lulus,Tingkakan lagi!"
+        ElseIf nilaiangka <= 70 Then
+            nilaihuruf = "B"
+            ket = "Nilai Anda Baik! Anda Lulus,Pertahankan! :)"
+        ElseIf nilaiangka <= 76 Then
+            nilaihuruf = "B+"
+            ket = "Nilai Anda Baik! Anda Lulus,Pertahankan! :)"
+        ElseIf nilaiangka >= 80 Then
+            nilaihuruf = "A"
+            ket = "Nilai Anda Sangat Baik! Anda Lulus,Selamat! :)"
+        End If
+        Texthuruf.Text = nilaihuruf
+        Textket.Text = ket
+    End Sub
+
+    Private Sub hapus_Click(sender As Object, e As EventArgs) Handles hapus.Click
+        Textnpm.Text = ""
+        Textnama.Text = ""
+        Texthadir.Text = ""
+        Texttugas.Text = ""
+        Textuts.Text = ""
+        Textuas.Text = ""
+        Textangka.Text = ""
+        Texthuruf.Text = ""
+        Textket.Text = ""
+    End Sub
+
+    Private Sub keluar_Click(sender As Object, e As EventArgs) Handles keluar.Click
+        Close()
+    End Sub
+End Class
